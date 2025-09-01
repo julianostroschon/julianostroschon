8bad37697e10fbfc6e32c74bcc07a981ce7e3f04
julianostroschon <julianostroschon@users.noreply.github.com>
2025-09-01 02:05:29 +0000

    Deploying to gh-pages from @ julianostroschon/julianostroschon@34d96f539d4783fd96d07871951c2341c7bf0790 🚀

--- js/repositories.js
+++ js/repositories.js
@@ -1,6 +1,20 @@
 // Dados dos repositórios
 const repositoriesData = [
   {
+    "name": "julianostroschon",
+    "technologies": [
+      "CSS",
+      "HTML",
+      "JavaScript"
+    ],
+    "updated": "2025-08-31",
+    "private": false,
+    "description": "Config files for my GitHub profile.",
+    "url": "https://github.com/julianostroschon/julianostroschon",
+    "stars": 2,
+    "forks": 0
+  },
+  {
     "name": "api-typescript-messages",
     "technologies": [
       "API",
@@ -15,20 +29,6 @@ const repositoriesData = [
     "description": "Repositório api-typescript-messages",
     "url": "https://github.com/julianostroschon/api-typescript-messages",
     "stars": 1,
-    "forks": 0
-  },
-  {
-    "name": "julianostroschon",
-    "technologies": [
-      "CSS",
-      "HTML",
-      "JavaScript"
-    ],
-    "updated": "2025-08-30",
-    "private": false,
-    "description": "Config files for my GitHub profile.",
-    "url": "https://github.com/julianostroschon/julianostroschon",
-    "stars": 2,
     "forks": 0
   },
   {
