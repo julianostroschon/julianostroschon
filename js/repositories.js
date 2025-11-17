d626ce3ffe210e9443226a93ef5223478899534c
julianostroschon <julianostroschon@users.noreply.github.com>
2025-11-17 01:57:22 +0000

    Deploying to gh-pages from @ julianostroschon/julianostroschon@a9a6eb0ea30eb45f91b981ee887b4041fa12a9c9 🚀

--- js/repositories.js
+++ js/repositories.js
@@ -7,7 +7,7 @@ const repositoriesData = [
       "HTML",
       "JavaScript"
     ],
-    "updated": "2025-11-15",
+    "updated": "2025-11-16",
     "private": false,
     "description": "Config files for my GitHub profile.",
     "url": "https://github.com/julianostroschon/julianostroschon",
