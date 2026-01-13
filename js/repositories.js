5ef5f5a340de6e99c90a46eb7186b540ba2f2d25
julianostroschon <julianostroschon@users.noreply.github.com>
2026-01-13 02:04:18 +0000

    Deploying to gh-pages from @ julianostroschon/julianostroschon@bd2877de87bf570345c428df14dbee5bfdaab53d 🚀

--- js/repositories.js
+++ js/repositories.js
@@ -1,6 +1,20 @@
 // Dados dos repositórios
 const repositoriesData = [
   {
+    "name": "julianostroschon",
+    "technologies": [
+      "CSS",
+      "HTML",
+      "JavaScript"
+    ],
+    "updated": "2026-01-12",
+    "private": false,
+    "description": "Config files for my GitHub profile.",
+    "url": "https://github.com/julianostroschon/julianostroschon",
+    "stars": 2,
+    "forks": 0
+  },
+  {
     "name": "devops",
     "technologies": [
       "Docker",
@@ -16,20 +30,6 @@ const repositoriesData = [
     "stars": "🔒",
     "forks": "🔒",
     "url": null
-  },
-  {
-    "name": "julianostroschon",
-    "technologies": [
-      "CSS",
-      "HTML",
-      "JavaScript"
-    ],
-    "updated": "2026-01-11",
-    "private": false,
-    "description": "Config files for my GitHub profile.",
-    "url": "https://github.com/julianostroschon/julianostroschon",
-    "stars": 2,
-    "forks": 0
   },
   {
     "name": "backup",
